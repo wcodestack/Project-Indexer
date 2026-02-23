7524f6512c38bb9199c3b6ad5a9bb4098cb4f9f7
wcodestack <wcodestack@users.noreply.github.com>
2026-02-23 14:02:32 +0530

    feat: add environment configuration support

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
